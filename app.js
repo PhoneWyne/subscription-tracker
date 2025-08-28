af9a95055f90bcdad588b71e6e1830c1cbd1733f
Muhammad Ahmed Hussain Wyne <85339083+PhoneWyne@users.noreply.github.com>
2025-08-28 17:34:20 +0500

    Feat/schemas 28 aug (#2)
    * feat: implemented subscription schema
    
    * feat: implemented error middleware
    
    * feat: finished integrating error middleware and added other middlewares

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 
 import { PORT } from './config/env.js';
 // routes
@@ -7,13 +8,23 @@ import subscriptionRouter from './routes/subscription.routes.js';
 import authRouter from './routes/auth.routes.js';
 
 import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
 
 const app = express();
+// this allows app to handle JSON body sent in request
+app.use(express.json());
+// this allows us to process form data sent via HTML in a simple format
+app.use(express.urlencoded({ extended: false}))
+// reads incoming cookies from requests
+app.use(cookieParser())
 
 // .use(path, Route), prepends path to  Route
 app.use('/api/v1/auth', authRouter);
 app.use(`/api/v1/users`, userRouter);
 app.use(`/api/v1/subscriptions`, subscriptionRouter);
+
+// .use() is used with routes, and middlewares
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
     res.send("Welcome to subscription tracker API!");
